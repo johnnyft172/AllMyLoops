e65bfb6cf08e11b0c26655673d731ee93c149741
jmintegr1 <jmintegr1@gmail.com>
2018-12-19 20:18:05 -0500

    All My Loops

---
+++ src/variationOfForLoop/WhileLoop.java
@@ -0,0 +1,19 @@
+package variationOfForLoop;
+
+public class WhileLoop {    //Count Controlled While Loop
+
+    public static void main(String[] agrs){
+
+        int i = 1;       //Here we initialize / start & CONDITION outside of the braces, unlike ForLoop
+        while(i <= 5)    //CONDITION
+        {
+            System.out.println(i + " "); //If we want to print out numbers
+
+            i++;   //Increment Or Decrement is inside the brace for code
+
+            /*System.out.println("I love purposeful focusing & coding as this is my suffering which lot easier than " +
+                    "hell week so I will jump in and attack it, eventually I will enjoy it!");
+*/
+        }
+    }
+}
---
+++ src/variationOfForLoop/incrementAndDecrementForLoop.java
@@ -0,0 +1,28 @@
+package variationOfForLoop;
+
+public class incrementAndDecrementForLoop {
+    public static void main(String[] args) {
+
+        /*for(int i = 1; i<=5; i++)        //Here increment by 1
+            System.out.print(i + " ");*/  //If just print then prints horizontally
+
+
+       /* for(int i = 5; i >= 1; i--)         //Now decrement so going down by 1
+            System.out.println(i + " ");*/   //If println then it prints vertically
+
+//        for(int i = 5; i < 25; i += 5)   //if you put ; after parenthesis then it will only read once
+//            System.out.print(i + " ");   //don't do it
+
+        /*for(int i = 1; i <= 5 ; i++)
+            System.out.println("I can enjoy good purposeful suffering because I attack it with heart!");*/
+            //Here we are typing text instead of number so replace i + " "  with statement to be printed
+
+
+        for(int i = 1; i <= 5; i++)
+        {
+            System.out.println("I love ligitimate suffering daily...");
+            System.out.println("Because this is the way to build callus in my brain and develop mental toughness!");
+            //If want to print more than 1 statement then put both of them in braces
+        }
+    }
+}
